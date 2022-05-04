a152cfb5ee5cc3683ce798d9ce5e328db39c9a3e
AlexFullOps <alexfullops@gmail.com>
2022-05-04 11:00:31 -0300

    second section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Serviços na área de TI',
+    headline: 'Projetos Web, Desktop e Mobile',
+    description: 'Criação e elaboração de sistemas intitucionais, Blogs, Lojas virtuais/e-commerces, Sites one-page, Hotsites, Landing Pages, Sites de conteúdo, Sistemas EAD, Portais de conteúdo e DIY',
+    buttonlabel: 'Saiba mais',
+    imgStart: false,
+    img: require('../../images/bc.gif'),
+    alt: 'Serviços prestados - Tech Front',
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,21 +1,53 @@
 import React from 'react'
+import { Button } from '../ButtonElements'
+import { 
+    InfoContainer,
+    InfoWrapper,
+    InfoRow, 
+    Column1,
+    Column2,
+    TextWrapper,
+    topLine,
+    Heading,
+    Subtitle,
+    BtnWrap,
+    ImgWrap,
+    Img
+} from './InfoElements'
 
-const InfoSection = () => {
+
+const InfoSection = ({  LightBg,
+                        id,
+                        imgStart,
+                        TopLine, 
+                        lightText, 
+                        headline, 
+                        darkText,
+                        description,
+                        buttonLabel,
+                        img,
+                        alt 
+                    }) => {
     return (
     <>
-    <InfoContainer id={id}>
+    <InfoContainer lightBg={LightBg} id={id}>
         <InfoWrapper>
-            <InfoRow>
+            <InfoRow imgStart={imgStart}>
                 <Column1>
                     <TextWrapper>
-                        <TopLine>TopLine</TopLine>
-                        <Heading>Heading</Heading>
-                        <Subtitle>Subtitle</Subtitle>
+                        <TopLine>{topLine}</TopLine>
+                        <Heading lightText={lightText}>{headline}</Heading>
+                        <Subtitle darkText={darkText}>{description}</Subtitle>
                         <BtnWrap>
-                            <Button to='home'>Button</Button>
+                            <Button to='home' >{buttonLabel}</Button>
                         </BtnWrap>
                     </TextWrapper>
                 </Column1>
+                <Column2>
+                    <ImgWrap>
+                        <Img src={img} alt={alt}/>
+                    </ImgWrap>
+                </Column2>
             </InfoRow>
         </InfoWrapper>
     </InfoContainer>
